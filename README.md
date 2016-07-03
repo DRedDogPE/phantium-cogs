dfab93c6df49bd14bc833a5a63fa0714d4bd6560
Phantium <phantium@users.noreply.github.com>
2016-07-03 23:32:12 +0200

    Add overwatch cog

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # phantium-cogs
-Cogs for Red
+Cogs for Red-DiscordBot
+
+overwatch.py
+=====
+Overwatch is my first cog project for Red-DiscordBot.
+It allows you to look up both Quick Play and Competitive profiles.
+Lookups are cached for 30 minutes before requesting fresh data from owapi.net.
--- overwatch.py
+++ overwatch.py
@@ -1,7 +1,17 @@
+"""
+Overwatch cog by Phantium
+https://github.com/phantium/phantium-cogs
+"""
+
 import discord
 import aiohttp
+from datetime import datetime
 from discord.ext import commands
 
+cache = None
+
+# Player data cache time
+cache_time = 1800
 
 class Overwatch:
     """Overwatch statistics."""
@@ -10,39 +20,93 @@ class Overwatch:
         self.bot = bot
         self.api_url = "https://owapi.net/api/v2/u/"
 
-    @commands.command(name="ow", aliases=["overwatch"])
-    async def ow(self, battletag):
-        """Gets overwatch quick play statistics. Usage: `[p]ow battletag#1234`"""
+    async def caching(self, ctx, type, battletag):
+        battletag = battletag.replace("#", "-")
+        battletag = battletag[0].upper() + battletag[1:]
+        user_api = self.api_url + battletag + "/stats/" + type
+
+        global cache
+        global cache_time
+
+        if not cache:
+            cache = {}
+        if not cache.get(type):
+            cache[type] = {}
+
+        if cache[type].get(battletag):
+
+            # Cleanup old cache items when the command is called
+            c_remove = {}
+            for count, (types, players) in enumerate(cache.items()):
+                for player, data in players.items():
+                    currenttime = ctx.message.timestamp
+                    elapsedtime = currenttime - datetime.strptime(data["updated"], '%Y-%m-%d %H:%M:%S.%f')
+                    if elapsedtime.seconds >= cache_time:
+                        c_remove[count] = {type: {"player": player}}
+
+            for c_index, c_expired in c_remove.items():
+                for c_type, c_battletag in c_expired.items():
+                    del cache[c_type][c_battletag["player"]]
+
+            if elapsedtime.seconds >= cache_time:
+                try:
+                    with aiohttp.ClientSession() as session:
+                        async with session.get(user_api) as api:
+                            ow = await api.json()
+
+                    cache[type][battletag] = {}
+                    cache[type][battletag]["updated"] = str(ctx.message.timestamp)
+                    cache[type][battletag]["data"] = {}
+                    cache[type][battletag]["data"] = ow
+                except:
+                    return {"error": 408, "msg": "Connection to Overwatch API timed out"}
+        else:
+            try:
+                with aiohttp.ClientSession() as session:
+                    async with session.get(user_api) as api:
+                        ow = await api.json()
+            except:
+                return {"error": 408, "msg": "Connection to Overwatch API timed out"}
+
+            if ow == 500:
+                return {"error": 500, "msg": "Internal Server Error"}
+
+            error = ow.get("error")
+            msg = ow.get("msg")
+            if error:
+                return {"error": error, "msg": msg}
+
+            cache[type][battletag] = {}
+            cache[type][battletag]["updated"] = str(ctx.message.timestamp)
+            cache[type][battletag]["data"] = {}
+            cache[type][battletag]["data"] = ow
+
+        return cache[type]
+
+
+    @commands.command(pass_context=True, name="ow", aliases=["overwatch"])
+    async def ow(self, ctx, battletag):
+        """Gets overwatch quick play statistics."""
 
         if "-" in battletag:
             await self.bot.say("Please specify `battletag#1234`")
             return
-        battletag = battletag.replace("#", "-")
-        user_api = self.api_url + battletag.capitalize() + "/stats/general"
 
-        try:
-            with aiohttp.ClientSession() as session:
-                async with session.get(user_api) as api:
-                    ow = await api.json()
-        except:
-            await self.bot.say("Unable to connect to Overwatch API.")
-            return
+        # Get quick play cache
+        cache = await self.caching(ctx, "general", battletag)
 
-        if ow == 500:
-            await self.bot.say("Unable to retrieve data from Overwatch API, error 500 (Internal Server Error).")
-            return
-
-        error = ow.get("error", "")
-        msg = ow.get("msg", "")
-        p_battletag = battletag.capitalize().replace("-", "#")
-
+        error = cache.get("error")
+        msg = cache.get("msg")
         if error:
-            await self.bot.say("Overwatch API returned: {} {}".format(error, msg))
+            await self.bot.say("Error {}: {}".format(error, msg.title()))
             return
 
-        owos = ow.get("overall_stats", None)
-        owgs = ow.get("game_stats", None)
+        battletag = battletag.replace("#", "-")
+        battletag = battletag[0].upper() + battletag[1:]
+        owos = cache[battletag]["data"]["overall_stats"]
+        owgs = cache[battletag]["data"]["game_stats"]
 
+        p_battletag = battletag.replace("-", "#")
         p_level = owos.get("level", 0)
         p_rank = str(owos.get("comprank", "0")).replace("None", "Unranked")
         p_kills = int(owgs.get("solo_kills", 0))
@@ -66,42 +130,29 @@ class Overwatch:
 
         await self.bot.say(message)
 
-    @commands.command(name="owc", aliases=["owcomp"])
-    async def owc(self, battletag):
-        """Gets overwatch competitive statistics. Usage: `[p]owc battletag#1234`"""
+    @commands.command(pass_context=True, name="owc", aliases=["owcomp"])
+    async def owc(self, ctx, battletag):
+        """Gets overwatch competitive statistics."""
 
         if "-" in battletag:
             await self.bot.say("Please specify `battletag#1234`")
             return
-        battletag = battletag.replace("#", "-")
-        user_api = self.api_url + battletag.capitalize() + "/stats/competitive"
 
-        try:
-            with aiohttp.ClientSession() as session:
-                async with session.get(user_api) as api:
-                    ow = await api.json()
-        except:
-            await self.bot.say("Unable to connect to Overwatch API.")
-            return
+        # Get competitive cache
+        cache = await self.caching(ctx, "competitive", battletag)
 
-        if ow == 500:
-            await self.bot.say("Unable to retrieve data from Overwatch API, error 500 (Internal Server Error).")
-            return
-
-        error = ow.get("error", "")
-        msg = ow.get("msg", "")
-        p_battletag = battletag.capitalize().replace("-", "#")
-
+        error = cache.get("error")
+        msg = cache.get("msg")
         if error:
-            if msg == "competitive stats not found":
-                await self.bot.say("Player {} has no stats on competitive, not enough games played.".format(p_battletag))
-                return
-            await self.bot.say("Overwatch API returned: {} {}".format(error, msg))
+            await self.bot.say("Error {}: {}".format(error, msg.title()))
             return
 
-        owos = ow.get("overall_stats", None)
-        owgs = ow.get("game_stats", None)
+        battletag = battletag.replace("#", "-")
+        battletag = battletag[0].upper() + battletag[1:]
+        owos = cache[battletag]["data"]["overall_stats"]
+        owgs = cache[battletag]["data"]["game_stats"]
 
+        p_battletag = battletag.replace("-", "#")
         p_level = owos.get("level", 0)
         p_rank = str(owos.get("comprank", "0")).replace("None", "Unranked")
         p_kills = int(owgs.get("solo_kills", 0))
